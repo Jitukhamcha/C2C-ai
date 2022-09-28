bcdb49b25f6a796e3252d394dce932d31dc2940a
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2022-09-28 23:28:18 +0545

    Tower of Hanoi

---
+++ Assignment 1/Tower of Hanoi.py
@@ -0,0 +1,15 @@
+disc_count=int(input("Enter desired amount of disc: "))
+
+def TowerOfHanoi(n,tw_source,tw_destination,tw_medium):
+    if(n>0):
+        #moving n-1 discs from A(source) to B(medium) using C(destination) recursively
+        TowerOfHanoi(n-1,tw_source,tw_medium,tw_destination)
+        #moving nth disc from A(source) to C(destination)
+        print(f"Move {n} from {tw_source} to {tw_destination} ")
+        #moving n-1 discs from B(medium) to C(Destination) using A(source) recursively
+        TowerOfHanoi(n-1,tw_medium,tw_destination,tw_source)
+        
+        
+        
+
+TowerOfHanoi(disc_count,'A','C','B')
