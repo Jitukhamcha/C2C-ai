4154f97a764518d4bd5772bfe2a8583828a21920
Sagar Lamichhane <the.rain54@gmail.com>
2022-09-29 16:59:15 +0545

    feedback given on count_character.py

--- assignmnet 1/count_character.py
+++ assignmnet 1/count_character.py
@@ -2,3 +2,5 @@ from collections import Counter
 string = 'this is the first assignment'
 repeated= Counter(string)
 print(repeated)
+''' Do not use Counter function to count the repetetion of characters in string. 
+Search other approaches '''
