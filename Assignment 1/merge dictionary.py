22fa3ee383ce2d9bca61bae813e65c25cefdf4f0
Srijansrijan <71896883+Srijansrijan@users.noreply.github.com>
2022-09-29 14:29:05 +0545

    Delete Assignment 1 directory

--- Assignment 1/merge dictionary.py
+++
@@ -1,9 +0,0 @@
-car = {"model":"Ferari","manu_date":2021,"engine":"Fuel injection"}
-bike = {"company":"Yamaha","purchase_date":2022,"Break":"ABS"} 
-scooty = {"Manufacturer":"honda","date":2020,"engine_cc":113}
-car.update(bike)
-car.update(scooty)
-print(car)
-#def merge(a,b,c):
-#    return(a.update(b))
-#print(merge(car,bike,car))
