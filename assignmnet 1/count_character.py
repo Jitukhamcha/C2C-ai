8726e3bb9959dd92df6e89cb3b086791586f60cc
unknown <Naslaa>
2022-09-29 19:07:38 +0545

    count characters made changes

--- assignmnet 1/count_character.py
+++ assignmnet 1/count_character.py
@@ -1,4 +1,11 @@
-from collections import Counter
-string = 'this is the first assignment'
-repeated= Counter(string)
-print(repeated)
+# initalizing string
+string="this is first assignment"
+all_char={} #creating dictionary
+#creating condition
+for i in string:
+	if i!= ' ' and i in all_char: # doesn't count space
+		all_char[i]+=1
+	else:
+		all_char[i]=1
+#printing result
+print('count', str(all_char))
