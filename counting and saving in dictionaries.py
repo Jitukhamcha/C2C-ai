128101e350f533d78bbca4b1d273511207debe62
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2022-10-12 12:59:52 +0545

    make changes to code after comments

--- counting and saving in dictionaries.py
+++ counting and saving in dictionaries.py
@@ -1,20 +1,14 @@
 #string
 line='My name is Bibek Thapa. I am CSIT student. My favourite animal is Zebra. Currently coding in python.'
-#alphabet to compare
-alphabet=[]
-for i in range (0,26):
-    alphabet.append(chr(i+97))
+#dict for  alphabets
+char_count={}
+for i in range(0,26):
+    char_count[chr(i+96)]=0 #ascii for lower aplhabets set as keys
+#now to count
+for ch in line.lower():
+    if(ch in char_count):
+        char_count[ch]+=1
 
-#counting character
-count_char={}
-for ch in line:
-    alp=ch.lower()
-    if (alp in alphabet):
-        if (alp in count_char):
-            count_char[alp]+=1
-        else:
-            count_char.update({alp:1})
-    
-print (count_char)
-    
+print(char_count)
+
 
